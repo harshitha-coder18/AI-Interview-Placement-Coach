620542a3a2f655915749f65b757e0872edb82d2c
harshitha <harshitha1656@gmail.com>
2026-07-26 12:04:46 +0530

    Day 5: Added React state and counter using useState

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
+import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
       <Navbar />
@@ -11,6 +14,19 @@ function App() {
         title="Crack Your Dream Placement 🚀"
         description="Practice DSA, build an ATS-friendly resume, and prepare for coding interviews with AI-powered guidance."
       />
+      <h2>Counter: {count}</h2>
+
+<button onClick={() => setCount(count + 1)}>
+  Increase
+</button>
+
+<button onClick={() => setCount(count - 1)}>
+  Decrease
+</button>
+
+<button onClick={() => setCount(0)}>
+  Reset
+</button>
     </>
   )
 }
