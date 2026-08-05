04b76168fe78da2260ba3d89d2004e026675bc11
harshitha <harshitha1656@gmail.com>
2026-08-05 20:57:56 +0530

    Day 16: Built user registration API using FastAPI and SQLite

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,9 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from .database import Base, engine, SessionLocal
+from .models import User
+from .schemas import UserCreate
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 def home():
     return {
         "message": "Welcome to AI Interview Placement Coach Backend 🚀"
+    }
+
+
+@app.post("/register")
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    new_user = User(
+        name=user.name,
+        email=user.email,
+        password=user.password
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {
+        "message": "User registered successfully",
+        "id": new_user.id
     }
